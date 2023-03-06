d1710384cf2873baa62ff4398779ee927309dfa9
Samuel Ghita <samuelghita1991@gmail.com>
2023-03-06 18:36:58 +0200

    Add findPrimeNumbers method

--- src/main/java/ro/samuel/drills/Exercices.java
+++ src/main/java/ro/samuel/drills/Exercices.java
@@ -2,6 +2,7 @@ package ro.samuel.drills;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 public class Exercices {
     public Integer getMaxNo(ArrayList<Integer> numbers) {
@@ -28,6 +29,27 @@ public class Exercices {
             return true;
         }
         return false;
-
     }
+
+    public Map<String, String> mapBully(Map<String, String> map) {
+        if (map.containsKey("a")) {
+            map.put("b", map.get("a"));
+            map.put("a", "");
+        }
+        return map;
+    }
+
+    public ArrayList<Integer> findPrimeNumbers(ArrayList<Integer> nr) {
+        ArrayList<Integer> prime = new ArrayList<>();
+        for (int i = 0; i < nr.size(); i++) {
+            for (int j = 2; j < nr.get(i); j++) {
+                if (nr.get(i) % j > 0) {
+                    prime.add(nr.get(i));
+                }
+            }
+
+        }
+        return prime;
+    }
+
 }
